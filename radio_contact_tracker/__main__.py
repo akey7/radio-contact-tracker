a229f1638ce1ba7313c4412852e3f8e6556b50e3
Alicia Key <alicia@akey7.com>
2020-10-21 12:36:25 -0600

    Input file
    Open the all.txt files and read its lines.

--- radio_contact_tracker/__main__.py
+++ radio_contact_tracker/__main__.py
@@ -1,7 +1,21 @@
 import argparse
+from typing import List, Dict, Union
+
+
+class RadioContactTracker:
+    def __init__(self, all_txt_filename: str):
+        self.all_txt_filename = all_txt_filename
+        self.all_txt_records: List[Dict[str, Union[str, float]]] = []
+
+    def read_all_txt(self) -> None:
+        with open(self.all_txt_filename, "r") as alt_txt:
+            for line in alt_txt.readlines():
+                print(line)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Process an ALL.TXT file from WSJT-X")
-    parser.add_argument("--input", type=str, help="Path to ALL.TXT from WSJT-X", required=True)
+    parser = argparse.ArgumentParser(description="Process an ALL.txt file from WSJT-X")
+    parser.add_argument("--input", type=str, help="Path to ALL.txt from WSJT-X", required=True)
     args = parser.parse_args()
+    tracker = RadioContactTracker(args.input)
+    tracker.read_all_txt()
