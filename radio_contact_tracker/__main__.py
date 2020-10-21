8ebd8f14b988208469ce91b6fa9fc8b3b44ed445
Alicia Key <alicia@akey7.com>
2020-10-21 07:36:40 -0600

    Command line arguments
    Add an argument to the path for --input

--- radio_contact_tracker/__main__.py
+++ radio_contact_tracker/__main__.py
@@ -1,2 +1,7 @@
+import argparse
+
+
 if __name__ == "__main__":
-    print("Hello world")
+    parser = argparse.ArgumentParser(description="Process an ALL.TXT file from WSJT-X")
+    parser.add_argument("--input", type=str, help="Path to ALL.TXT from WSJT-X", required=True)
+    args = parser.parse_args()
